8578260f40224e0188105eb40699beae60ec53ce
Lương Vinh <luongthevinh.dev@gmail.com>
2021-10-25 14:53:38 +0700

    backup ts

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -18,24 +18,8 @@ AOS.init({
 import * as fetchData from "./components/fetchData";
 
 // Layout
-import {banner} from "./layout/banner";
-import {popup} from "./layout/popup";
-import {search} from "./layout/search";
-import {header} from "./layout/header";
-import {dropdown} from "./layout/dropdown";
 
 // Page
-import {index} from "./pages/index";
-import {careersSubpages} from "./pages/careersSubpages";
-import {careersSubchildren} from "./pages/careersSubchildren";
-import {careersJob} from "./pages/careersJob";
-import {farmSubchildren} from "./pages/farmSubchildren";
-import {subPages} from "./pages/subpages";
-import {contactPage} from "./pages/contact";
-import {newsPage} from "./pages/news";
-import {aboutNetwork} from "./pages/about-network";
-import {deploy} from "./pages/deploy";
-import {sustainPage} from "./pages/Sustain";
 
 // import { interpolate } from "gsap/all";
 
@@ -46,27 +30,12 @@ function common(){
 }
 
 function layout(){
-  banner();
-  popup();
-  search();
-  header();
-  dropdown();
+
 }
 
 // function Page
 function page(){
   // ----- add page in here ----- //
-  index();
-  careersSubpages();
-  careersSubchildren();
-  farmSubchildren();
-  careersJob();
-  subPages();
-  contactPage();
-  newsPage();
-  aboutNetwork();
-  deploy();
-  sustainPage();
 }
 
 
