f74ee00fdbf6a17073164c9d6839649157429596
Lương Vinh <luongthevinh.dev@gmail.com>
2021-10-27 14:31:56 +0700

    members ss2

--- src/scripts/pages/village.ts
+++ src/scripts/pages/village.ts
@@ -2,13 +2,16 @@ function handleMouseCardDocument(){
   var cards = document.querySelectorAll('.card-document');
   if (cards.length !== 0){
     cards.forEach(function(item){
-      item.addEventListener('mouseleave',function(){
-        var behind = <HTMLElement>item.querySelector('.effect-behind');
-        behind.style.animation = "outBehind .9s ease-in-out forwards";
-      })
+      // mouse hover
       item.addEventListener('mouseover',function(){
         var behind = <HTMLElement>item.querySelector('.effect-behind');
         behind.style.animation = "moveBehind .9s ease-in-out forwards";
+      })
+
+      // mouse leave
+      item.addEventListener('mouseleave',function(){
+        var behind = <HTMLElement>item.querySelector('.effect-behind');
+        behind.style.animation = "outBehind .9s ease-in-out forwards";
       })
     })
   }
