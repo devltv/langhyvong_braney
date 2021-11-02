b1aff21721087f87d1e5b65d4d56c9e4d25a039a
Lương Vinh <luongthevinh.dev@gmail.com>
2021-11-02 16:07:29 +0700

    fix popup

--- src/scripts/layout/_popup.ts
+++ src/scripts/layout/_popup.ts
@@ -1,61 +1,86 @@
-function hiddenScroll(){
-  var body = <HTMLElement> document.querySelector('body');
-  body.style.height = '100vh';
-  body.style.overflow = 'hidden';
+declare var Swiper : any;
+
+function hiddenScroll() {
+  var body = <HTMLElement>document.querySelector('body');
+  body.classList.add('hiddenScroll');
 }
 
-function visibleScroll(){
-  var body = <HTMLElement> document.querySelector('body');
-  body.style.height = '';
-  body.style.overflow = '';
+function visibleScroll() {
+  var body = <HTMLElement>document.querySelector('body');
+  body.classList.remove('hiddenScroll');
 }
 
-function handlePopup(){
+function swiperPopup(idxKey:any) {
+  var popup = new Swiper("#popupImg.active .swiper-popup", {
+    slidesPerView: 1,
+    spaceBetween: 16,
+    navigation: {
+      nextEl: "#popupImg.active .swiper-button-next",
+      prevEl: "#popupImg.active .swiper-button-prev",
+    },
+  });
+
+  popup.slideTo(idxKey, false, false);
+}
+
+function handlePopup() {
   var imgs = document.querySelectorAll('.members1--wrapper .myStudent .myStudent__slide');
   var idPopup = document.getElementById('popupImg');
+  var wrapperPopup = document.querySelector('#popupImg .swiper-popup .swiper-wrapper');
 
-  imgs.forEach(function(item){
+  imgs.forEach(function (item, idxKey) {
     var btn = item.querySelector('img');
     if (btn){
-      btn.addEventListener('click', function(){
-        var srcImg = btn.getAttribute('src');
-        var imgPopup = idPopup.querySelector('.popupImg-container--ratio');
-        imgPopup.setAttribute('src', srcImg);
+      btn.addEventListener('click', function () {
+        btn.classList.add('clicked');
+        var wrapperStudent = document.querySelector('#members1 .myStudent .swiper-wrapper');
+        if (wrapperPopup && wrapperStudent){
+          wrapperPopup.innerHTML = wrapperStudent.innerHTML;
+        }
+
+        // var srcImg = btn.getAttribute('src');
+        // var imgPopup = idPopup.querySelector('.popupImg-container--ratio');
+        // if (imgPopup) {
+        //   imgPopup.setAttribute('src', srcImg);
+        // }
+        
         hiddenScroll();
-        idPopup.classList.add('active');
         idPopup.style.animation = "popupRoom .2s ease-in-out forwards";
+        idPopup.classList.add('active');
+        swiperPopup(idxKey);
       })
     }
   })
-  if (idPopup){
-    var close = <HTMLElement> idPopup.querySelector('.close');
-    if (close){
-      close.onclick = function(e){
+  if (idPopup) {
+    var close = <HTMLElement>idPopup.querySelector('.close');
+    if (close) {
+      close.onclick = function (e) {
         e.preventDefault();
         idPopup.classList.remove('active');
         idPopup.style.display = "block";
         idPopup.style.animation = "popupClose .2s ease-in-out forwards";
+        wrapperPopup.innerHTML = "";
         visibleScroll();
       }
     }
   }
 }
 
-function handlePopup360deg(){
+function handlePopup360deg() {
   var deg = document.getElementById('deg360');
-  if (deg){
+  if (deg) {
     var btn = deg.querySelector('.deg360');
     var idPopup = document.getElementById('popup360deg');
-    if (btn && idPopup){
-      btn.addEventListener('click', function(){
+    if (btn && idPopup) {
+      btn.addEventListener('click', function () {
         hiddenScroll();
         idPopup.classList.add('active');
         idPopup.style.animation = "popupRoom .2s ease-in-out forwards";
       })
 
-      var close = <HTMLElement> idPopup.querySelector('.close');
-      if (close){
-        close.onclick = function(e){
+      var close = <HTMLElement>idPopup.querySelector('.close');
+      if (close) {
+        close.onclick = function (e) {
           e.preventDefault();
           visibleScroll();
           idPopup.classList.remove('active');
@@ -65,13 +90,14 @@ function handlePopup360deg(){
       }
     }
 
-    if (idPopup){
-      
+    if (idPopup) {
+
     }
   }
 }
 
-export const _popup = function(){
+export const _popup = function () {
   handlePopup();
   handlePopup360deg();
+
 }
